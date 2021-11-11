8361daf726a816a04860796dcaeffdccb8b8b3b4
mmalay <malaydsg@gmail.com>
2021-11-12 01:20:07 +0530

    joinee

--- components/Join/TeamUpItem.tsx
+++ components/Join/TeamUpItem.tsx
@@ -1,15 +1,40 @@
 import { useAuth } from '@/context/authContext';
+import { JoinPostType } from '@/utilities/join/JoinPostType';
+import { db } from 'firebase/firebaseClient';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import FixedButton from '../UI/Buttons/FixedButton';
 
-export default function TeamUpItem() {
+type Props = {
+  data: JoinPostType;
+};
+
+export default function Post({ data }: Props) {
   const {
-    userData: { username, name, photoURL },
-  } = useAuth(); //TODO: remove this
+    userData: { uid, username, name, photoURL },
+  } = useAuth();
   const router = useRouter();
+
   const openProfile = () => {
-    router.push(`/profile/${username}`);
+    router.push(`/profile/${data.username}`);
+  };
+
+  const onClick = () => {
+    router.push(`/join/${data.uid}/${data.id}`);
+  };
+
+  const createJoinee = async () => {
+    if (data.uid !== uid) {
+      await db
+        .collection('join')
+        .doc(data.id)
+        .collection('joinees')
+        .doc(uid)
+        .set({ uid, username, name, photoURL })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   return (
@@ -18,16 +43,17 @@ export default function TeamUpItem() {
         'xl:w-1/2 md:w-5/6 mx-auto font-sans px-3 md:px-12 pb-8 pt-4 ' +
         'bg-gray-900 rounded-lg my-2 '
       }
+      onClick={onClick}
     >
       <div className="flex items-center justify-between px-3 mb-8 rounded-lg border-2 border-gray-800 ">
         <section
           className="flex gap-3 items-center justify-start "
           onClick={openProfile}
         >
-          {photoURL ? (
+          {data.photoURL ? (
             <div className="relative h-10 w-10 md:h-14 md:w-14 cursor-pointer">
               <Image
-                src={photoURL}
+                src={data.photoURL}
                 alt="Picture of a friend"
                 layout="fill"
                 objectFit="contain"
@@ -37,66 +63,70 @@ export default function TeamUpItem() {
           ) : null}
           <section className="flex flex-col cursor-pointer">
             <span className="font-semibold text-sm  md:text-lg text-gray-300 hover:text-indigo-600">
-              {name}
+              {data.name}
             </span>
             <span className="font-semibold text-gray-500 text-xs md:text-base">
-              @{username}
+              @{data.username}
             </span>
           </section>
         </section>
-        <FixedButton
-          name={`Join ${username}`}
-          onClick={() => {
-            console.log('join');
-          }}
-        />
+        {data.uid !== uid ? (
+          <FixedButton name={`Join ${data.username}`} onClick={createJoinee} />
+        ) : null}
       </div>
       <span className="text-gray-100 tracking-wide pt-1">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur optio
-        incidunt et ducimus sequi explicabo ipsam enim quasi expedita eligendi
-        non numquam dolor labore, harum libero? Nesciunt dolorum reprehenderit
-        eius.
+        {data.description}
       </span>
       <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-2 mt-8 space-5 gap-6">
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">Game</span>
-          <span className="text-gray-100 tracking-wide pt-1">BGMI</span>
+          <span className="text-gray-100 tracking-wide pt-1">
+            {data.gameCode}
+          </span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">Mode</span>
-          <span className="text-gray-100 tracking-wide pt-1">TPP</span>
+          <span className="text-gray-100 tracking-wide pt-1">{data.mode}</span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">K/D</span>
-          <span className="text-gray-100 tracking-wide pt-1">6</span>
+          <span className="text-gray-100 tracking-wide pt-1">{data.kd}</span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">
             Average Damage
           </span>
-          <span className="text-gray-100 tracking-wide pt-1">2kg</span>
+          <span className="text-gray-100 tracking-wide pt-1">
+            {data.averageDamage}
+          </span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">Age</span>
-          <span className="text-gray-100 tracking-wide pt-1">24</span>
+          <span className="text-gray-100 tracking-wide pt-1">{data.age}</span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">
             Experience
           </span>
-          <span className="text-gray-100 tracking-wide pt-1">1 year</span>
+          <span className="text-gray-100 tracking-wide pt-1">
+            {data.experience}
+          </span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">Role</span>
-          <span className="text-gray-100 tracking-wide pt-1">Sniper</span>
+          <span className="text-gray-100 tracking-wide pt-1">{data.role}</span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">Purpose</span>
-          <span className="text-gray-100 tracking-wide pt-1">Rank up</span>
+          <span className="text-gray-100 tracking-wide pt-1">
+            {data.purpose}
+          </span>
         </section>
         <section className="flex flex-col">
           <span className="font-semibold text-gray-500 text-sm">Language</span>
-          <span className="text-gray-100 tracking-wide pt-1">Hindi</span>
+          <span className="text-gray-100 tracking-wide pt-1">
+            {data.language}
+          </span>
         </section>
       </div>
     </div>
--- pages/join/[uid]/index.tsx
+++ pages/join/[uid]/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useAuth } from '@/context/authContext';
 import Head from 'next/head';
 import TeamUpItem from '../../../components/Join/TeamUpItem';
+import { db } from 'firebase/firebaseClient';
+import { JoinPostType } from '@/utilities/join/JoinPostType';
 
 export default function MyPosts() {
+  const [posts, setPosts] = useState<JoinPostType[]>([]);
+  const {
+    userData: { uid },
+  } = useAuth();
+
+  useEffect(() => {
+    const getPosts = async () => {
+      db.collection('join')
+        .where('uid', '==', uid)
+        .get()
+        .then((querySnapshot) => {
+          const posts: JoinPostType[] = [];
+          querySnapshot.forEach((doc) => {
+            posts.push({ ...(doc.data() as JoinPostType), id: doc.id });
+          });
+          setPosts(posts);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getPosts();
+  }, [uid]);
+
   return (
     <div className="flex flex-col sm:static w-full sm:px-10 px-0">
       <Head>
@@ -11,12 +39,12 @@ export default function MyPosts() {
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <div>
-        <div>
-          <TeamUpItem />
-          <TeamUpItem />
-          <TeamUpItem />
-          <TeamUpItem />
-          <TeamUpItem />
+        <div className="mt-8">
+          {posts.map((post) => (
+            <div key={post.id}>
+              <TeamUpItem data={post} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
